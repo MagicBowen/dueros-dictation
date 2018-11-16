1a3d730b77bed03167a2bd30c7d7317be33869b9
magicbowen <e.bowen.wang@icloud.com>
2018-11-16 11:19:21 +0800

    modify

--- bot.js
+++ bot.js
@@ -89,7 +89,8 @@ class Bot extends BaseBot {
             return {
                 // directives: [new BaseBot.Directive.AudioPlayer.Stop()],
                 // directives: [this.getTextTemplate(`写完了，可以对我说：“小度小度，下一个”。`)],
-                outputSpeech: `<speak>， <silence time="5s"></silence></speak>`
+                // outputSpeech: `<speak>， <silence time="5s"></silence></speak>`
+                outputSpeech: `写好了，对我说：下一个`
             }
         });
 
